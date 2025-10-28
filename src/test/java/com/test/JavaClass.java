606b53e57d336880c2134801d4803b969519c58c
Gautam2303 <gowtham6306@gmail.com>
2025-10-28 15:01:15 +0530

    new master repo created

---
+++ src/test/java/com/test/JavaClass.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class JavaClass {
+	private void testJava() {
+		System.out.println("Java");
+	}
+	private void testAPI() {
+		System.out.println("API");
+	}
+	private void testTestNG() {
+		System.out.println("TestNG");
+	}
+	private void testJUnit() {
+		System.out.println("JUnit");
+	}
+
+}
